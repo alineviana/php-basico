50bfe99ac5526ca4b0634409f7431be9c45f8d4e
Aline Viana <80078418+alineviana@users.noreply.github.com>
2023-10-20 13:52:31 -0300

    add example

--- text/example.php
+++ text/example.php
@@ -1,2 +1,21 @@
 <?php
 
+$nome = "Vinícius Dias";
+
+$ehDaMinhaFamilia = str_contains($nome, 'Dias');
+
+if ($ehDaMinhaFamilia) {
+    echo "$nome é da minha família!" . PHP_EOL;
+} else {
+    echo "$nome não é da minha família!" . PHP_EOL;
+}
+
+$url = 'https://alura.com.br';
+
+if (str_starts_with($url, 'https')) {
+    echo "É uma URL segura!" . PHP_EOL;
+} else {
+    echo "Não é uma URL segura!" . PHP_EOL;
+}
+
+$email = 'vinicius@alura.com.br';
